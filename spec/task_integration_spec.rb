8f5f313d851ae764f291c3af90040ba67b7c96a3
jocelyn alsdorf & jeanette fairless <student@epicodus.com>
2015-04-28 10:45:12 -0700

    test integration specs for path for to-do list; web application loads and svaes properly

---
+++ spec/task_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('path to to-do list', {:type => :feature}) do
+  it('user inputs a task and it is saved to the to-do list array') do
+    visit('/')
+    fill_in('description', :with => 'sweep floors')
+    click_button('Add task')
+    expect(page).to have_content('The task has been successfully submitted!')
+  end
+end
